b7acf786e5260b2cced2b453c0dab130cb3b5c0a
Thais <tay.valade@gmail.com>
2020-07-07 11:41:56 +0200

    av

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
@@ -12,10 +14,19 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/registration", name="registration")
      */
-    public function registration()
+    public function registration(Request $request,EntityManagerInterface $manager)
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class,$user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+ 
+          
+            $manager->persist($user);
+            $manager->flush();
+ 
+           // return $this->redirectToRoute('connexion');
+        }
         return $this->render('registration/registration.html.twig', [
             'form' => $form->createView(),
         ]);
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,7 +19,7 @@ class User implements UserInterface
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=180, unique=true)
+     * @ORM\Column(type="string", length=180)
      */
     private $email;
 
